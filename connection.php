0949747a432ada05f2c533ff8ca1f8840d61c14a
Suresh1206 <90610462+Suresh1206@users.noreply.github.com>
2021-12-03 16:13:36 +0530

    Add files via upload

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
     $servername='localhost';
     $username='root';
     $password='';
-    $dbname='blood_group';
+    $dbname='kmit';
     $conn=new mysqli($servername,$username,$password,$dbname);
     if($conn->connect_error){
         die("Connection error :".$conn->connect_error);
